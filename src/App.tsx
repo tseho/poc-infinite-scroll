0c8705ea570eb21492c146626689a575787484c5
Tseho <quentin.favrie@gmail.com>
2021-03-15 16:05:09 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
+import useScrollPosition, {ScrollPosition} from './scroll/useScrollPosition';
+import useScrollPositionWithoutParent from './scroll/useScrollPositionWithoutParent';
 
 function App() {
+  const rows = Array.from(Array(100).keys());
+  const ref = useRef(null);
+
+  useScrollPosition(ref, (position: ScrollPosition) => {
+    console.log('with parent', position);
+  }, [], 300);
+
+  useScrollPositionWithoutParent(ref, (position: ScrollPosition) => {
+    console.log('without parent', position);
+  }, [], 300);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      alignItems: "stretch",
+      border: "1px solid blue",
+      boxSizing: "border-box",
+      display: "flex",
+      flexDirection: "column",
+      height: "calc(100% - 10px)",
+      width: "300px",
+      overflowY: "auto",
+    }}>
+      <div>
+        <ul ref={ref}>
+          {rows.map(row => (
+            <li
+              key={row}
+              style={{
+                background: "lightgray",
+                height: "40px",
+                border: "1px solid #fff",
+                display: "block",
+              }}>
+              {row}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
